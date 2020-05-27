e8a1a9954fb6ddf59d2bea5b283134ae9697dada
Sh Shanker <shivashankerbhatta@gmail.com>
2020-05-27 11:01:27 +0545

    initial push

---
+++ functions.php
@@ -0,0 +1,122 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if (!function_exists('twentyseventeen_locale_css')):
+    function twentyseventeen_locale_css($uri)
+    {
+        if (empty($uri) && is_rtl() && file_exists(get_template_directory() . '/rtl.css'))
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter('locale_stylesheet_uri', 'twentyseventeen_locale_css');
+
+if (!function_exists('twentyseventeen_parent_css')):
+    function twentyseventeen_parent_css()
+    {
+        wp_enqueue_style('twentyseventeen_parent', trailingslashit(get_template_directory_uri()) . 'style.css', array());
+    }
+endif;
+add_action('wp_enqueue_scripts', 'twentyseventeen_parent_css', 10);
+
+// END ENQUEUE PARENT ACTION
+
+//BEGIN NEW CUSTOMIZER PANEL
+/**
+ * Customizer additions.
+ */
+require get_stylesheet_directory() . '/inc/customizer.php';
+
+
+//BEGIN DISPLAYING LATEST POST
+if (!function_exists('twentyseventeen_latest_posts_on_sidebar')):
+    function twentyseventeen_latest_posts_on_sidebar()
+    {
+
+
+        // Get layout.
+        $show_latest_posts_section = get_theme_mod('twentyseventeen_show_latest_posts');
+
+        if ($show_latest_posts_section == false) {
+            return;
+        }
+
+        $latest_posts_section_title = get_theme_mod('twentyseventeen_latest_posts_section_title');
+        $latest_posts_category = get_theme_mod('twentyseventeen_latest_posts_category');
+        $show_latest_posts_category = get_theme_mod('twentyseventeen_show_latest_posts_category');
+
+        $all_posts = twentyseventeen_get_latest_posts(3, $latest_posts_category);
+
+        ?>
+        <section id="latest-posts-on-sidebar" class="widget widget_recent_entries group-blog">
+            <h2 class="widget-title">
+                <?php echo esc_html($latest_posts_section_title); ?>
+            </h2>
+            <ul>
+                <?php
+                if ($all_posts->have_posts()) :
+                    while ($all_posts->have_posts()) : $all_posts->the_post();
+                        global $post;
+                        ?>
+                        <li>
+
+                            <?php the_post_thumbnail(); ?>
+
+                            <?php if ($show_latest_posts_category == 'yes'): ?>
+                                <div class="entry-meta">
+                                    <?php echo get_the_category_list($post->ID); ?>
+                                </div>
+                            <?php endif; ?>
+
+                            <h4><a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+
+                            <div class="entry-meta">
+                                <?php twentyseventeen_posted_on(); ?>
+                            </div>
+
+
+                        </li>
+                    <?php
+                    endwhile;
+                    wp_reset_postdata();
+                endif;
+
+                ?>
+            </ul>
+        </section>
+
+        <?php
+    }
+endif;
+add_action('twentyseventeen_latest_posts_on_sidebar_action', 'twentyseventeen_latest_posts_on_sidebar', 10);
+
+
+function twentyseventeen_get_latest_posts($number_of_posts = 3, $category = 0)
+{
+    $ins_args = array(
+        'post_type' => 'post',
+        'posts_per_page' => absint($number_of_posts),
+        'post_status' => 'publish',
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'ignore_sticky_posts' => true
+    );
+
+    $category = isset($category) ? $category : '0';
+
+    if (absint($category) > 0) {
+        $ins_args['cat'] = absint($category);
+    }
+
+    $all_posts = new WP_Query($ins_args);
+
+    return $all_posts;
+
+}
---
+++ inc/customizer.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function twentyseventeen_latest_posts_on_sidebar_register( $wp_customize ) {
+
+    // Load customize extra controls.
+    require get_stylesheet_directory().'/inc/customizer-controls.php';
+
+    // Load customize extra sanitize.
+    require get_stylesheet_directory().'/inc/customizer-sanitize.php';
+    
+
+    /**
+     * New Customizer options panel.
+     * Section - latest posts section
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_section
+     */
+
+    $wp_customize->add_section(
+        'latest_posts_on_sidebar',
+        array(
+            'title'    => __( 'Latest Posts on Sidebar', 'twentyseventeen' ),
+            'priority' => 100,
+        )
+    );
+
+
+    /**
+     * New Customizer checkbox settings for the panel.
+     * Setting - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_setting
+     */
+
+    $wp_customize->add_setting('twentyseventeen_show_latest_posts',
+        array(
+            'default' => true,
+            'capability' => 'edit_theme_options',
+            'sanitize_callback' => 'twentyseventeen_sanitize_checkbox', //check inc/customizer-sanitize for custom sanitize function.
+        )
+    );
+
+    /**
+     * New Customizer checkbox control for the panel.
+     * Controls - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_control
+     *
+     */
+
+    $wp_customize->add_control('twentyseventeen_show_latest_posts',
+        array(
+            'label' => __('Show Latest Posts on Sidebar', 'twentyseventeen'),
+            'description' => esc_html__('This is a checkbox', 'twentyseventeen'),
+            'section' => 'latest_posts_on_sidebar',
+            'type' => 'checkbox',
+            'priority' => 100,
+        )
+    );
+
+
+    /**
+     * New Customizer texts input settings for the panel.
+     * Setting - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_setting
+     */
+
+    $wp_customize->add_setting(
+        'twentyseventeen_latest_posts_section_title',
+        array(
+            'default'           => esc_html__('Latest Posts', 'twentyseventeen'),
+            'capability' => 'edit_theme_options',
+            'sanitize_callback' => 'sanitize_text_field',
+            
+        )
+    );
+
+
+    /**
+     * New Customizer input texts control for the panel.
+     * Controls - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_control
+     *
+     */
+    $wp_customize->add_control(
+        'twentyseventeen_latest_posts_section_title',
+        array(
+            'label' => esc_html__('Section Title', 'twentyseventeen'),
+            'description' => esc_html__('This is a texts input', 'twentyseventeen'),
+            'section' => 'latest_posts_on_sidebar',
+            'type' => 'text',
+            'priority' => 100,
+            'active_callback' => 'twentyseventeen_show_latest_posts_callback'
+        )
+    );
+
+    /**
+     * New Customizer category dropdown settings for the panel.
+     * Setting - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_setting
+     */
+    $wp_customize->add_setting('twentyseventeen_latest_posts_category',
+        array(
+            'default' => 0,
+            'capability' => 'edit_theme_options',
+            'sanitize_callback' => 'absint',
+        )
+    );
+
+
+
+    /**
+     * New Customizer custom category control for the panel.
+     * Controls - latest posts section.
+     * TwentySeventeen_Dropdown_Taxonomies_Control - check custom taxonomy control on inc/customizer-controls
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_control
+     *
+     */
+    $wp_customize->add_control(new TwentySeventeen_Dropdown_Taxonomies_Control($wp_customize, 'twentyseventeen_latest_posts_category',
+        array(
+            'label' => esc_html__('Category', 'twentyseventeen'),
+            'description' => esc_html__('This is a category dropdown', 'twentyseventeen'),
+            'section' => 'latest_posts_on_sidebar',
+            'type' => 'dropdown-taxonomies',
+            'taxonomy' => 'category',
+            'priority' => 100,
+            'active_callback' => 'twentyseventeen_show_latest_posts_callback'
+
+        )));
+
+
+
+    /**
+     * New Customizer radio input  settings for the panel.
+     * Setting - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_setting
+     */
+    $wp_customize->add_setting( 'twentyseventeen_show_latest_posts_category', array(
+        'default' => 'yes',
+        'capability' => 'edit_theme_options',
+        'sanitize_callback' => 'twentyseventeen_sanitize_select',
+    ) );
+
+
+    /**
+     * New Customizer custom category control for the panel.
+     * Controls - latest posts section.
+     * https://codex.wordpress.org/Class_Reference/WP_Customize_Manager/add_control
+     *
+     */
+    $wp_customize->add_control( 'twentyseventeen_show_latest_posts_category', array(
+        'type' => 'radio',
+        'section' => 'latest_posts_on_sidebar', // Add a default or your own section
+        'label' => __( 'Show Categories' ),
+        'description' => __( 'This is a custom radio input.' ),
+        'choices' => array(
+            'yes' => __( 'Yes' ),
+            'no' => __( 'No' ),
+        ),
+        'priority' => 100,
+        'active_callback' => 'twentyseventeen_show_latest_posts_callback'
+    ) );
+
+
+
+}
+//https://codex.wordpress.org/Theme_Customization_API
+add_action( 'customize_register', 'twentyseventeen_latest_posts_on_sidebar_register' );
