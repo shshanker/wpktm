1972971e21f7b8668bb3b5837674b8782ce2c7f7
Sh Shanker <shivashankerbhatta@gmail.com>
2020-05-30 21:52:39 +0545

    setting-2 updated

--- inc/customizer-sanitize.php
+++ inc/customizer-sanitize.php
@@ -17,29 +17,3 @@ if ( ! function_exists( 'twentyseventeen_sanitize_checkbox' ) ) :
     }
 
 endif;
-
-if ( ! function_exists( 'twentyseventeen_sanitize_select' ) ) :
-
-    /**
-     * Sanitize select.
-     *
-     * @since 1.0.0
-     *
-     * @param mixed                $input The value to sanitize.
-     * @param WP_Customize_Setting $setting WP_Customize_Setting instance.
-     * @return mixed Sanitized value.
-     */
-    function twentyseventeen_sanitize_select( $input, $setting ) {
-
-        // Ensure input is a slug.
-        $input = sanitize_text_field( $input );
-
-        // Get list of choices from the control associated with the setting.
-        $choices = $setting->manager->get_control( $setting->id )->choices;
-
-        // If the input is a valid key, return it; otherwise, return the default.
-        return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
-
-    }
-
-endif;
